43ed7bbd3169ea5e69841d702d41c65fe0f883a3
kittyzmm <gelia.andreevna@yandex.ru>
2025-11-13 15:00:50 +0300

    Update Add main structure

--- main.cpp
+++ main.cpp
@@ -100,3 +100,87 @@ bool isMagicSquare(int** matrix, int n) {
     if (diag2 != targetSum) return false;
     return true;
 }
+
+void printMatrix(int** matrix, int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void inputMatrix(int** matrix, int n) {
+    cout << "Введите элементы матрицы (" << n << "x" << n << "):" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "");
+    int n;
+    cout << "Размер квадратной матрицы (n): ";
+    cin >> n;
+    int** matrix = createMatrix(n);
+    inputMatrix(matrix, n);
+    int choice;
+    do {
+        cout << "\nМеню:" << endl;
+        cout << "1. Повернуть на 90°" << endl;
+        cout << "2. Повернуть на 180°" << endl;
+        cout << "3. Повернуть на 270°" << endl;
+        cout << "4. Найти макс. сумму подматрицы k x k" << endl;
+        cout << "5. Проверить, магический ли квадрат" << endl;
+        cout << "6. Вывести матрицу" << endl;
+        cout << "0. Выход" << endl;
+        cout << "Ваш выбор: ";
+        cin >> choice;
+        switch (choice) {
+        case 1:
+            rotateMatrix90(matrix, n);
+            cout << "Матрица повернута на 90°." << endl;
+            printMatrix(matrix, n);
+            break;
+        case 2:
+            rotateMatrix180(matrix, n);
+            cout << "Матрица повернута на 180°." << endl;
+            printMatrix(matrix, n);
+            break;
+        case 3:
+            rotateMatrix270(matrix, n);
+            cout << "Матрица повернута на 270°." << endl;
+            printMatrix(matrix, n);
+            break;
+        case 4: {
+            int k;
+            cout << "Размер подматрицы k: ";
+            cin >> k;
+            int maxSum = findMaxSubmatrixSum(matrix, n, k);
+            cout << "Максимальная сумма подматрицы " << k << "x" << k << ": " << maxSum << endl;
+            break;
+        }
+        case 5: {
+            bool magic = isMagicSquare(matrix, n);
+            if (magic) {
+                cout << "Матрица является магическим квадратом!" << endl;
+            }
+            else {
+                cout << "Матрица НЕ является магическим квадратом." << endl;
+            }
+            break;
+        }
+        case 6:
+            printMatrix(matrix, n);
+            break;
+        case 0:
+            break;
+        default:
+            cout << "Неверный выбор. Попробуйте снова." << endl;
+        }
+    } while (choice != 0);
+    deleteMatrix(matrix, n);
+    return 0;
+}
